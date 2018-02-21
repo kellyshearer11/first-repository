d82a25300e0c7c072a27be36542b73a360b9c204
kellyshearer11 <36700066+kellyshearer11@users.noreply.github.com>
2018-02-21 13:17:35 +0000

    Create menu.java

---
+++ menu.java
@@ -0,0 +1,16 @@
+// for github lab
+
+import java.util.Scanner;
+
+  public class Menu{
+    public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    
+        System.out.println("Select an option (1-4):");
+        System.out.println();
+        System.out.println("1. If..else");
+        System.out.println("2. While");
+        System.out.println("3. For");
+         System.out.println("4. Switch");
+           System.out.println();
+        
